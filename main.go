959d3fed2616883c61637292cafb9331453171f0
takeshi-shiihara <shi4490@icloud.com>
2019-10-21 18:21:07 +0900

    ビュー

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"github.com/zenazn/goji/web/middleware"
-	"github.com/jinzhu/gorm"
-	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *gorm.DB
+var db gorm.DB
 
 func main() {
 	user := web.New()
-	gogi.Handle("/user/*", user)
+	goji.Handle("/user/*", user)
 
 	user.Use(middleware.SubRouter)
 	user.Use(SuperSecure)
@@ -24,9 +23,5 @@ func main() {
 	user.Post("/update/:id", UserUpdate)
 	user.Get("/delete/:id", UserDelete)
 
-	gogi.Server()
-}
-
-func init {
-	db, _ = gorm.Open("mysql", "root@/gorm?charset=utf8&parseTime=True")
+	goji.Serve()
 }
